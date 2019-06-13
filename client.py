61f7a359c9fdcfaf7f9787828073e818fc658dbf
Elle <eabraddy@gmail.com>
2019-06-13 14:24:09 -0400

    Update UI to have user list on left side

--- client.py
+++ client.py
@@ -43,33 +43,46 @@ def on_closing(event=None):
 top = tkinter.Tk()
 top.title("Talkative")
 
-messages_frame = tkinter.Frame(top)
+# Frame declarations
+talkative_frame = tkinter.Frame(top)
+user_frame = tkinter.Frame(talkative_frame)
+messages_frame = tkinter.Frame(talkative_frame)
+
+talkative_frame.pack(fill=tkinter.BOTH, expand=1)
 myFont = tkinter.font.Font(family='Helvetica', size=11)
 
 my_msg = tkinter.StringVar()  # For the messages to be sent.
 my_msg.set("Enter message...")
 
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+scrollbar1 = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
 
 # Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, yscrollcommand=scrollbar.set, height=20, width=100)
+msg_list = tkinter.Listbox(messages_frame, yscrollcommand=scrollbar1.set, height=20, width=75)
 msg_list.config(font=myFont, bg='#36393f', fg='#c8c9cb')
-scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=1)
-msg_list.pack()
-messages_frame.pack(fill=tkinter.BOTH, expand=1)
+scrollbar1.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+messages_frame.pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=1)
 
-entry_field = tkinter.Entry(top, textvariable=my_msg, font=myFont,
+scrollbar2 = tkinter.Scrollbar(user_frame)  # To navigate through currently connected users.
+
+# Connected user list
+user_list = tkinter.Listbox(user_frame, yscrollcommand=scrollbar2.set, height=20, width=25)
+user_list.config(font=myFont, bg='#36393f', fg='#c8c9cb')
+scrollbar2.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+user_list.pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=1)
+user_frame.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=1)
+
+# User input field and entry button
+entry_field = tkinter.Entry(messages_frame, textvariable=my_msg, font=myFont,
                             insertbackground='#c8c9cb', bg='#484c52', fg='#c8c9cb')
 entry_field.bind("<FocusIn>", lambda args: entry_field.delete('0', 'end'))
 entry_field.bind("<Return>", send)
 entry_field.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=1)
 # Enter button
-send_button = tkinter.Button(top, font=myFont, text="Send", command=send, bg='#484c52', fg='#c8c9cb')
-send_button.pack(ipadx=5, ipady=5, side=tkinter.LEFT, fill=tkinter.BOTH)
+send_button = tkinter.Button(messages_frame, font=myFont, text="Send", command=send, bg='#484c52', fg='#c8c9cb')
+send_button.pack(ipadx=5, ipady=5, side=tkinter.RIGHT, fill=tkinter.BOTH)
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
-
 
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect(ADDR)
