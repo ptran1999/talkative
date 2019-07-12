45ccb1500e32fead26b2d3d9a326c6aa77891025
Kevin Tran <kevintrn99@gmail.com>
2019-07-12 11:36:52 -0400

    added userdb.py :p

---
+++ userDB.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+class userDB:
+
+    def __init__(self):
+        self.conn = sqlite3.connect("USERS.db")
+        self.mycursor = self.conn.cursor()
+
+        self.mycursor.execute("""CREATE TABLE IF NOT EXISTS USERS (
+                    _ID INTEGER PRIMARY KEY,
+                    USERNAME TEXT,
+                    PASSWORD TEXT )""")
+
+    # Inserts a new user into the database
+    def user_insert(self, username, password):
+        self.mycursor.execute("INSERT INTO USERS (USERNAME, PASSWORD) VALUES (?,?)", (username,password) )
+        self.conn.commit()
+
+    # Searches a for a username in the the database
+    def username_query(self, username):
+        self.mycursor.execute("SELECT * FROM USERS WHERE USERNAME =?", (username, ))
+        return self.mycursor.fetchall()
+
+    # Searches for a user with the specified username and password
+    def user_query(self, username, password):
+        self.mycursor.execute("SELECT * FROM USERS WHERE USERNAME = ? AND PASSWORD = ?",
+                              (username, password))
+        return self.mycursor.fetchall()
+
+    # Get User ID
+    def get_user_id(self, username):
+
+        self.mycursor.execute("SELECT _ID FROM USERS WHERE USERNAME = ?",
+                              (username, ))
+        return self.mycursor.fetchall()
+
+    # TODO Implement all friends functions
+
+
+
