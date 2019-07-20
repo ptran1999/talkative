811b3ca898d0012f364c0ead275b1e91c6d00197
Phat Tran <34192355+chuoibuoituoi@users.noreply.github.com>
2019-07-20 15:57:45 -0400

    Update userDB.py

--- userDB.py
+++ userDB.py
@@ -13,8 +13,8 @@ class userDB:
 
         self.mycursor.execute("""CREATE TABLE IF NOT EXISTS RELATIONSHIP (
                             id INTEGER PRIMARY KEY,
-                            user1_ID TEXT,
-                            user2_ID TEXT,
+                            username_1 TEXT,
+                            username_2 TEXT,
                             status TEXT )""")
 
     # Inserts a new user into the database
@@ -40,7 +40,36 @@ class userDB:
                               (username, ))
         return self.mycursor.fetchall()
 
-    # TODO Implement all friends functions
+    # Inserts a new relation
+    def rela_insert(self, username1, username2):
+        self.mycursor.execute("INSERT INTO RELATIONSHIP (username_1, username_2, status) VALUES (?,?,0)", (username1, username2) )
+        self.conn.commit()
 
+    # Accept friend request
+    def accept_req(self, username1, username2):
+        self.mycursor.execute("UPDATE relationship SET status = 1 WHERE username_1 = ? and username_2 = ?",
+                              (username1, username2))
 
+    # Decline friend request
+    def decline_req(self, username1, username2):
+        self.mycursor.execute("DELETE FROM relationship WHERE username_1 = ? and username_2 = ?",
+                              (username1, username2))
+
+    # Display friend list
+    def get_friendList(self, username1):
+        self.mycursor.execute("SELECT username FROM USERS "
+                              "WHERE username IN "
+                              "(SELECT username_2 FROM relationship WHERE username_1 = ? AND status = 1)"
+                              " OR username IN "
+                              "(SELECT username_1 FROM relationship WHERE username_2 = ? AND status = 1)",
+                              (username1, username1))
+        return self.mycursor.fetchall()
+
+    # Display friend request list1
+    def get_requestList(self, username2):
+        self.mycursor.execute("SELECT username From USERS "
+                              "WHERE username IN "
+                              "(SELECT username_1 FROM relationship WHERE username_2 = ? AND status = 0)",
+                              (username2) )
+        return self.mycursor.fetchall()
 
